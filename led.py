01be8c39d9ad9efe6852cbafe99a823d65dd844b
Shad0w27 <shad0w27.66242007@gmail.com>
2024-12-04 20:44:59 +0000

    fixed the led logic

--- led.py
+++ led.py
@@ -1,7 +1,13 @@
 from gpiozero import LED
 
+# Store LED objects for each pin
+led_objects = {}
+
 def ledOn(pin):
-    LED(pin).on()
-    
+    if pin not in led_objects:
+        led_objects[pin] = LED(pin)  # Create LED object for the pin
+    led_objects[pin].on()  # Turn on the LED
+
 def ledOff(pin):
-    LED(pin).off()
+    if pin in led_objects:
+        led_objects[pin].off()  # Turn off the LED
